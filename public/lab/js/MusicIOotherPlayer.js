99dade5c4cc6a4f30f7ce311a6be71ab2e5ccbab
lihbr <lihbr@users.noreply.github.com>
2018-03-12 22:38:36 +0100

    :sparkles: Adding Three.js

---
+++ public/lab/js/MusicIOotherPlayer.js
@@ -0,0 +1,77 @@
+/**
+ * MusicIO other player gestion
+ */
+class MusicIOotherPlayer {
+  constructor ({
+    ctx,
+    player
+  } = {}) {
+
+    // Global var
+    this._ctx = ctx
+    this._player = player
+    console.log(player)
+    // Player pos
+    this._pos = player.position
+    // Player model
+    this._mesh = new THREE.Object3D()
+    this._parts = {}
+
+    // Init player
+    this.init()
+    console.log("new player")
+  }
+
+  /**
+   * Init everything
+   */
+  init() {
+    // Create player shape
+    this.create()
+  }
+
+  // Sub init method
+
+  // Create the player 3D model
+  create () {
+    // Player 3D container setup
+    this._ctx.scene.add(this._mesh)
+    this._mesh.position.x = this._pos.x
+    this._mesh.position.y = this._pos.y
+    this._mesh.position.z = 5
+
+
+    // Global material
+    const material =  new THREE.MeshStandardMaterial({
+      color: this._player.color,
+      metalness: 0.3,
+      roughness: 0,
+      flatShading: true
+    })
+
+    // Body
+    const body = new THREE.Mesh(
+      new THREE.SphereGeometry( 10, 8, 6 ),
+      material
+    )
+    // Add body to player container
+    this._parts.body = body
+    this._mesh.add(this._parts.body)
+  }
+
+  /**
+   * Update everything
+   */
+  update (position) {
+    this._pos.x = position.x - this._ctx.terrain.w / 2
+    this._pos.y = position.y - this._ctx.terrain.h / 2
+
+    // Update player position
+    this._mesh.position.x = this._pos.x
+    this._mesh.position.y = this._pos.y
+  }
+
+  die () {
+    this._ctx.scene.remove(this._mesh)
+  }
+}
---
+++ public/lab/js/main.js
@@ -0,0 +1,56 @@
+// Init
+let fxaa = parseInt(window.prompt("Turn on anti-alliasing ? 0/1"))
+if (fxaa != 1) {
+  fxaa = false
+}
+let ssao = parseInt(window.prompt("Turn on ambient ? 0/1"))
+if (ssao != 1) {
+  ssao = false
+}
+let devTools = parseInt(window.prompt("Devtools ? 0/1"))
+if (devTools != 0) {
+  devTools = true
+}
+const room = window.prompt("Room name ?")
+const name = window.prompt("Player name ?")
+/* const fxaa = false
+const ssao = false
+const room = 999
+const name = Math.floor(Math.random() * 1000) */
+//const name= `${Date.now()}${Math.floor(Math.random() * Date.now())}`
+
+const rngColorGen = () => {
+  let s = "0x"
+  const c = "0123456789ABCDEF"
+  for (let i = 0; i < 6; i++) {
+    s += c.charAt(Math.floor(Math.random() * c.length))
+  }
+  return parseInt(s)
+}
+
+const rngColor = rngColorGen()
+console.log(rngColor)
+
+// Init socket
+const socket = io()
+socket.emit("join", room, name, rngColor)
+
+const game = new MusicIO({
+  canvas : ".musicIO",
+  fxaa: fxaa,
+  ssao: ssao,
+  color: rngColor
+})
+
+
+// Update pos
+socket.on('updatePlayer', (players) => {
+  game.socketUpdate(players)
+  if (devTools) {
+    drawPlayers(players)
+  }
+})
+
+/* socket.on('aPlayerPlaySound', (player) => {
+    aPlayerPlaySound(player)
+}) */
