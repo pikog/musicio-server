6370a702c0a2dd93220dd19c7990337856a50924
edhbr <edouard.haberer@gmail.com>
2018-05-21 03:16:34 +0200

    :sparkles: home v1.0

--- public/dev/assets/js/main.js
+++ public/dev/assets/js/main.js
@@ -1,15 +1,1 @@
-// Toggle room choice
-const radio = document.querySelectorAll(".joinForm [type='radio']")
-const auto = document.querySelector(".joinForm #auto")
-const optional = document.querySelector(".joinForm .optional")
-if (radio && auto && optional) {
-  const checkSignup = () => {
-    auto.checked ? optional.classList.remove("active") : optional.classList.add("active")
-  }
-  for (let i = 0; i < radio.length; i++) {
-    radio[i].addEventListener("change", checkSignup)
-  }
-  checkSignup()
-}
-
 const game = new MusicIO(".musicIOApp")
