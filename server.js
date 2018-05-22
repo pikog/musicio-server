d6c4bbfcb3cad21c516b53bdf52d88dcbb97d645
Pikog <perso.pikog@gmail.com>
2018-05-23 00:45:10 +0200

    :ok_hand:Pass all code with 2 indents

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 8080;
 //Start server
 http.listen(port, () =>
 {
-    console.log(`Server started on port ${port}`)
+  console.log(`Server started on port ${port}`)
 })
 
 //Init socket.io
@@ -24,11 +24,11 @@ app.use('/static', express.static(__dirname + '/public'))
 //Default route
 app.use((req, res, next) =>
 {
-    res.redirect(`/static/dev/`)
+  res.redirect(`/static/dev/`)
 })
 
 //Socket Event listener
 io.on('connection', (socket) =>
 {
-    connection.onConnection(socket)
+  connection.onConnection(socket)
 })
