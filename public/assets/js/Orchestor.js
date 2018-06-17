e36226b3b12378260dac0271e268c16135eeab0a
lihbr <lihbr@users.noreply.github.com>
2018-06-17 17:52:13 +0200

    :wrench::construction: earlier metronome state

--- public/assets/js/Orchestor.js
+++ public/assets/js/Orchestor.js
@@ -381,7 +381,7 @@ class Orchestor {
       for (const instr in movement) {
         for (const input in movement[instr]) {
           this._ctx._loading.total++
-          this._metronomeState = 1
+          this._metronomeState = 2
         }
       }
     }
